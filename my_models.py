b649751c77444738ef6ad1381da9cb39a25abce5
Stanislav <56927835+Stanislavius@users.noreply.github.com>
2023-10-09 00:46:49 +0300

    REFACTOR creating class from type
    Create class NaiveBayes using type instead of class definition via "class n: ...."

--- my_models.py
+++ my_models.py
@@ -1,25 +1,32 @@
 import numpy as np
 import collections
 
-class Naive_Bayes:
-    def __init__(self):
-        self.apriori = None
-        self.freq = None
+def init(self):
+    self.apriori = None
+    self.freq = None
 
-    def fit(self, X: np.array, y: list):
-        if self.apriori is None and self.freq is None:
-            self.apriori = np.array(list(dict(sorted(collections.Counter(y).items())).values()))
-            self.freq = np.zeros(shape = (len(self.apriori), len(X[0])))
+def fit(self, X: np.array, y: list):
+    if self.apriori is None and self.freq is None:
+        self.apriori = np.array(list(dict(sorted(collections.Counter(y).items())).values()))
+        self.freq = np.zeros(shape = (len(self.apriori), len(X[0])))
         
-        for sample, class_num in zip(X, y):
-            self.freq[class_num] += sample
-        #TODO: add recount appriori for new data or maybe not
+    for sample, class_num in zip(X, y):
+        self.freq[class_num] += sample
+    #TODO: add recount appriori for new data or maybe not
 
-        self.freq = np.transpose(np.transpose(self.freq) / self.apriori)
-        self.apriori = self.apriori / self.apriori.sum()
+    self.freq = np.transpose(np.transpose(self.freq) / self.apriori)
+    self.apriori = self.apriori / self.apriori.sum()
         
-    def predict(self, X: np.array):
-        result = np.zeros(shape = (len(X)))
-        for i in range(len(X)):
-            result[i] = ((self.freq * X[i]).sum(axis = 1) * self.apriori).argmax()
-        return result
+def predict(self, X: np.array):
+    result = np.zeros(shape = (len(X)))
+    for i in range(len(X)):
+        result[i] = ((self.freq * X[i]).sum(axis = 1) * self.apriori).argmax()
+    return result
+
+Naive_Bayes = type("NaiveBayes", (object, ), { 
+    "__init__": init, 
+    
+      
+    "fit": fit, 
+    "predict": predict
+}) 
